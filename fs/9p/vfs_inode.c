739ee1ce171fa07d029c433b7ad369a54be854ad
Al Viro <viro@zeniv.linux.org.uk>
2011-06-25 19:15:54 -0400

    don't transliterate lower bits of ->intent.open.flags to FMODE_...
    ->create() instances are much happier that way...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/9p/vfs_inode.c
+++ fs/9p/vfs_inode.c
@@ -713,7 +713,7 @@ v9fs_vfs_create(struct inode *dir, struct dentry *dentry, umode_t mode,
 	fid = NULL;
 	v9ses = v9fs_inode2v9ses(dir);
 	perm = unixmode2p9mode(v9ses, mode);
-	if (nd)
+	if (nd && nd->flags & LOOKUP_OPEN)
 		flags = nd->intent.open.flags;
 	else
 		flags = O_RDWR;
