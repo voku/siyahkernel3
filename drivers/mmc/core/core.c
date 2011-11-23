9b55dcd9d73d13bb1e591da6f02c56457d29fd9b
Sujit Reddy Thumma <sthumma@codeaurora.org>
2011-11-23 08:43:18 +0530

    mmc: core: Fix deadlock when the CONFIG_MMC_UNSAFE_RESUME is not defined
    mmc_suspend_host() tries to claim host during suspend
    and release it only when the bus suspend operation is
    compeleted. If CONFIG_MMC_UNSAFE_RESUME is defined and
    the host is flagged as removable, mmc_suspend_host()
    tries to remove the card. In this process, the file system
    sync can get blocked trying to acquire host which is already
    claimed by mmc_suspend_host() causing deadlock.
    
    Fix this deadlock by releasing host before ->remove() is called.
    
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    Acked-by: Ulf Hansson <ulf.hansson@stericsson.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2366,6 +2366,8 @@ int mmc_suspend_host(struct mmc_host *host)
 				mmc_poweroff_notify(host);
 				err = host->bus_ops->suspend(host);
 			}
+			mmc_do_release_host(host);
+
 			if (err == -ENOSYS || !host->bus_ops->resume) {
 				/*
 				 * We simply "remove" the card in this case.
@@ -2380,8 +2382,6 @@ int mmc_suspend_host(struct mmc_host *host)
 				host->pm_flags = 0;
 				err = 0;
 			}
-
-			mmc_do_release_host(host);
 		} else {
 			err = -EBUSY;
 		}
