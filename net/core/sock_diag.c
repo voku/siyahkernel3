2fd53ab700bd2fa2b699182e741a335b130c077d
Dan Carpenter <dan.carpenter@oracle.com>
2011-12-07 20:49:38 +0000

    sock_diag: off by one checks
    These tests are off by one because sock_diag_handlers[] only has AF_MAX
    elements.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Acked-by: Pavel Emelyanov <xemul@parallels.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/sock_diag.c
+++ net/core/sock_diag.c
@@ -51,7 +51,7 @@ int sock_diag_register(const struct sock_diag_handler *hndl)
 {
 	int err = 0;
 
-	if (hndl->family > AF_MAX)
+	if (hndl->family >= AF_MAX)
 		return -EINVAL;
 
 	mutex_lock(&sock_diag_table_mutex);
@@ -69,7 +69,7 @@ void sock_diag_unregister(const struct sock_diag_handler *hnld)
 {
 	int family = hnld->family;
 
-	if (family > AF_MAX)
+	if (family >= AF_MAX)
 		return;
 
 	mutex_lock(&sock_diag_table_mutex);
