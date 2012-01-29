0ffb22acbb1622e38f8762fc0f4b61fefaa72f97
Alex Frid <afrid@nvidia.com>
2012-01-29 20:39:25 +0100

    PM / QoS: Simplify PM QoS expansion/merge
     - Replace class ID #define with enumeration
     - Loop through PM QoS objects during initialization (rather than
       initializing them one-by-one)
    
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-by: Antti Miettinen <amiettinen@nvidia.com>
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Acked-by: markgross <markgross@thegnar.org>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -525,35 +525,18 @@ static ssize_t pm_qos_power_write(struct file *filp, const char __user *buf,
 static int __init pm_qos_power_init(void)
 {
 	int ret = 0;
+	int i;
 
-	ret = register_pm_qos_misc(&cpu_dma_pm_qos);
-	if (ret < 0) {
-		printk(KERN_ERR "pm_qos_param: cpu_dma_latency setup failed\n");
-		return ret;
+	BUILD_BUG_ON(ARRAY_SIZE(pm_qos_array) != PM_QOS_NUM_CLASSES);
+
+	for (i = 1; i < PM_QOS_NUM_CLASSES; i++) {
+		ret = register_pm_qos_misc(pm_qos_array[i]);
+		if (ret < 0) {
+			printk(KERN_ERR "pm_qos_param: %s setup failed\n",
+			       pm_qos_array[i]->name);
+			return ret;
+		}
 	}
-	ret = register_pm_qos_misc(&network_lat_pm_qos);
-	if (ret < 0) {
-		printk(KERN_ERR "pm_qos_param: network_latency setup failed\n");
-		return ret;
-	}
-	ret = register_pm_qos_misc(&network_throughput_pm_qos);
-	if (ret < 0)
-		printk(KERN_ERR
-			"pm_qos_param: network_throughput setup failed\n");
-	ret = register_pm_qos_misc(&bus_dma_throughput_pm_qos);
-	if (ret < 0)
-		printk(KERN_ERR
-			"pm_qos_param: bus_dma_throughput setup failed\n");
-
-	ret = register_pm_qos_misc(&display_frequency_pm_qos);
-	if (ret < 0)
-		printk(KERN_ERR
-			"pm_qos_param: display_frequency setup failed\n");
-
-	ret = register_pm_qos_misc(&dvfs_res_lat_pm_qos);
-	if (ret < 0)
-		printk(KERN_ERR
-			"pm_qos_param: dvfs_response_frequency setup failed\n");
 
 	return ret;
 }
