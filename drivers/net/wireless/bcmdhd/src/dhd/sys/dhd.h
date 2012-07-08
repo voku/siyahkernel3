8d2c213e7be47e4936cb79f2ca80359783a4706f
Lars Moelleken <voku@voku-online.de>
2012-07-08 18:36:29 +0200

    + sync our changes with "https://github.com/sakuramilk/sc02c_kernel_ics/pull/10/commits" [need testing]

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd.h
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd.h
@@ -242,9 +242,6 @@ typedef struct dhd_pub {
 	char eventmask[WL_EVENTING_MASK_LEN];
 	int	op_mode;				/* STA, HostAPD, WFD, SoftAP */
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) && defined(CONFIG_HAS_WAKELOCK)
-	struct wake_lock wakelock[WAKE_LOCK_MAX];
-#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) && defined (CONFIG_HAS_WAKELOCK) */
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 25)) && 1
 	struct mutex	wl_start_stop_lock; /* lock/unlock for Android start/stop */
 	struct mutex	wl_softap_lock;		 /* lock/unlock for any SoftAP/STA settings */
@@ -340,7 +337,8 @@ void dhd_os_spin_unlock(dhd_pub_t *pub, unsigned long flags);
 extern int dhd_os_wake_lock(dhd_pub_t *pub);
 extern int dhd_os_wake_unlock(dhd_pub_t *pub);
 extern int dhd_os_wake_lock_timeout(dhd_pub_t *pub);
-extern int dhd_os_wake_lock_timeout_enable(dhd_pub_t *pub, int val);
+extern int dhd_os_wake_lock_rx_timeout_enable(dhd_pub_t *pub, int val);
+extern int dhd_os_wake_lock_ctrl_timeout_enable(dhd_pub_t *pub, int val);
 
 inline static void MUTEX_LOCK_SOFTAP_SET_INIT(dhd_pub_t * dhdp)
 {
@@ -363,37 +361,11 @@ inline static void MUTEX_UNLOCK_SOFTAP_SET(dhd_pub_t * dhdp)
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) */
 }
 
-#ifdef DHD_DEBUG_WAKE_LOCK
-#define DHD_OS_WAKE_LOCK(pub) \
-	do { \
-		printf("call wake_lock: %s %d\n", \
-			__func__, __LINE__); \
-		dhd_os_wake_lock(pub); \
-	} while (0)
-#define DHD_OS_WAKE_UNLOCK(pub) \
-	do { \
-		printf("call wake_unlock: %s %d\n", \
-			__func__, __LINE__); \
-		dhd_os_wake_unlock(pub); \
-	} while (0)
-#define DHD_OS_WAKE_LOCK_TIMEOUT(pub) \
-	do { \
-		printf("call wake_lock_timeout: %s %d\n", \
-			__func__, __LINE__); \
-		dhd_os_wake_lock_timeout(pub); \
-	} while (0)
-#define DHD_OS_WAKE_LOCK_TIMEOUT_ENABLE(pub, val) \
-	do { \
-		printf("call wake_lock_timeout_enable[%d]: %s %d\n", \
-			val, __func__, __LINE__); \
-		dhd_os_wake_lock_timeout_enable(pub, val); \
-	} while (0)
-#else
 #define DHD_OS_WAKE_LOCK(pub)			dhd_os_wake_lock(pub)
 #define DHD_OS_WAKE_UNLOCK(pub)			dhd_os_wake_unlock(pub)
 #define DHD_OS_WAKE_LOCK_TIMEOUT(pub)		dhd_os_wake_lock_timeout(pub)
-#define DHD_OS_WAKE_LOCK_TIMEOUT_ENABLE(pub, val)	dhd_os_wake_lock_timeout_enable(pub, val)
-#endif /* DHD_DEBUG_WAKE_LOCK */
+#define DHD_OS_WAKE_LOCK_RX_TIMEOUT_ENABLE(pub, val)  dhd_os_wake_lock_rx_timeout_enable(pub, val)
+#define DHD_OS_WAKE_LOCK_CTRL_TIMEOUT_ENABLE(pub, val)  dhd_os_wake_lock_ctrl_timeout_enable(pub, val)
 #define DHD_PACKET_TIMEOUT_MS	1000
 #define DHD_EVENT_TIMEOUT_MS	1500
 
