d2680d9926555ca884621d15a04263c8556ad77b
root <root@voku-VirtualBox.(none)>
2012-07-09 16:10:40 +0200

    + AFTR Idle Mode is working ... so lets test AFTR + LPA Idle Mode :)

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -825,20 +825,22 @@ static int exynos4_enter_lowpower(struct cpuidle_device *dev,
 		__raw_writel(tmp, S5P_CENTRAL_SEQ_OPTION);
 	}
 
-    if (new_index == 0)
+	if (new_index == 0) {
     		printk(KERN_INFO "Info: starting Idle Mode!\n");
 		return exynos4_enter_idle(dev, drv, new_index);
-	else
+	}
+
+	enter_mode = exynos4_check_entermode();
+	if (!enter_mode) {
+		printk(KERN_INFO "Info: starting Idle Mode!\n");
+		return exynos4_enter_idle(dev, drv, new_index);
+	} else if (enter_mode == S5P_CHECK_DIDLE) {
 		printk(KERN_INFO "Info: starting AFTR Idle Mode!\n");
 		return exynos4_enter_core0_aftr(dev, drv, new_index);
-
-	//enter_mode = exynos4_check_entermode();
-	//if (!enter_mode)
-	//	return exynos4_enter_idle(dev, drv, new_index);
-	//else if (enter_mode == S5P_CHECK_DIDLE)
-	//	return exynos4_enter_core0_aftr(dev, drv, new_index);
-	//else
-	//	return exynos4_enter_core0_lpa(dev, drv, new_index);
+	} else {
+		printk(KERN_INFO "Info: starting LPA Idle Mode!\n");
+		return exynos4_enter_core0_lpa(dev, drv, new_index);
+	}
 }
 
 static int exynos4_cpuidle_notifier_event(struct notifier_block *this,
