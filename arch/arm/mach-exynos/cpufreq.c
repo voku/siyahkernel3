902e8f0b66bdd4579a53c5a1cf342f881826cae5
Dorimanx <yuri@bynet.co.il>
2012-07-09 15:30:10 +0300

    Revert "mach-exynos: cpufreq: Let performance governor wake up at policy max"
    This reverts commit 0313e1ad90c614de41f3c91a5a2e6d4886f20803.

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -568,16 +568,6 @@ static int exynos_cpufreq_notifier_event(struct notifier_block *this,
 	int ret = 0;
 	unsigned int cpu = 0;
 	struct cpufreq_policy *policy = cpufreq_cpu_get(cpu);
-	unsigned int safe_arm_volt, arm_volt;
-	unsigned int *volt_table;
-	unsigned int max_index;
-
-	volt_table = exynos_info->volt_table;
-	policy = cpufreq_cpu_get(0);
-
-	exynos_cpufreq_get_level(policy->max, &max_index);
-	if (unlikely(!policy))
-		exynos_cpufreq_get_level(exynos_getspeed(0), &max_index);
 
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
@@ -609,25 +599,6 @@ static int exynos_cpufreq_notifier_event(struct notifier_block *this,
 #if defined(CONFIG_CPU_EXYNOS4210)
 		exynos_cpufreq_upper_limit_free(DVFS_LOCK_ID_PM);
 #endif
-		// In case of using performance governor,
-		// max level should be used after sleep and wakeup
-		if (exynos_cpufreq_lock_disable) {
-			mutex_lock(&set_freq_lock);
-
-			/* get the voltage value */
-			safe_arm_volt = exynos_get_safe_armvolt(exynos_info->pm_lock_idx, max_index);
-			if (safe_arm_volt)
-				regulator_set_voltage(arm_regulator, safe_arm_volt,
-					safe_arm_volt + 25000);
-
-			arm_volt = volt_table[max_index];
-			regulator_set_voltage(arm_regulator, arm_volt,
-				arm_volt + 25000);
-
-			exynos_info->set_freq(exynos_info->pm_lock_idx, max_index);
-
-			mutex_unlock(&set_freq_lock);
-		}
 		exynos_cpufreq_disable = false;
 		/* If current governor is userspace or performance or powersave,
 		 * restore the saved cpufreq after waekup.
