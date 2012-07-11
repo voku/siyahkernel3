72df61a7b0cc64aaa8cc49590b61011ec675797d
Dorimanx <yuri@bynet.co.il>
2012-07-11 18:07:58 +0300

    Code Fix after revert.

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -125,7 +125,6 @@ struct menu_device {
 #define LOAD_INT(x) ((x) >> FSHIFT)
 #define LOAD_FRAC(x) LOAD_INT(((x) & (FIXED_1-1)) * 100)
 
-#if 0
 static int get_loadavg(void)
 {
 	unsigned long this = this_cpu_load();
