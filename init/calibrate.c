436709bcd13458026390f42b52bf50b68754d6b4
Dorimanx <yuri@bynet.co.il>
2012-07-12 17:50:16 +0300

    Revert "init:calibrate: lpj from 3.4."
    This reverts commit 910b7a02450ac3cfcd37f8551e2d60eb12ea96c0.

--- init/calibrate.c
+++ init/calibrate.c
@@ -246,20 +246,6 @@ recalibrate:
 
 static DEFINE_PER_CPU(unsigned long, cpu_loops_per_jiffy) = { 0 };
 
-/*
- * Check if cpu calibration delay is already known. For example,
- * some processors with multi-core sockets may have all cores
- * with the same calibration delay.
- * 
- * 
- * Architectures should override this function if a faster calibration
- * method is available.
- */
-unsigned long __attribute__((weak)) __cpuinit calibrate_delay_is_known(void)
-{
-	return 0;
-}
-
 void __cpuinit calibrate_delay(void)
 {
 	unsigned long lpj;
@@ -268,9 +254,8 @@ void __cpuinit calibrate_delay(void)
 
 	if (per_cpu(cpu_loops_per_jiffy, this_cpu)) {
 		lpj = per_cpu(cpu_loops_per_jiffy, this_cpu);
-		if (!printed)
-		  pr_info("Calibrating delay loop (skipped) "
-		    "already calibrated this CPU");
+		pr_info("Calibrating delay loop (skipped) "
+				"already calibrated this CPU");
 	} else if (preset_lpj) {
 		lpj = preset_lpj;
 		if (!printed)
@@ -280,8 +265,6 @@ void __cpuinit calibrate_delay(void)
 		lpj = lpj_fine;
 		pr_info("Calibrating delay loop (skipped), "
 			"value calculated using timer frequency.. ");
-	} else if ((lpj = calibrate_delay_is_known())) {
-		;
 	} else if ((lpj = calibrate_delay_direct()) != 0) {
 		if (!printed)
 			pr_info("Calibrating delay using timer "
