79baf2a7932a0c0deb1d58fb7a5518b7b0bb51d1
Dorimanx <yuri@bynet.co.il>
2012-07-12 12:46:54 +0300

    Fixed last commit and removed test change.

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -121,10 +121,10 @@ struct menu_device {
 	int		interval_ptr;
 };
 
-#if 0
 #define LOAD_INT(x) ((x) >> FSHIFT)
 #define LOAD_FRAC(x) LOAD_INT(((x) & (FIXED_1-1)) * 100)
 
+#if 0
 static int get_loadavg(void)
 {
 	unsigned long this = this_cpu_load();
