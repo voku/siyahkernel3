d2cd0e0ed163e4cc3af4e05353a1e71b8a313b0e
Johan Hedberg <johan.hedberg@intel.com>
2012-07-16 16:12:02 +0300

    Bluetooth: Initial skeleton for Three-wire UART (H5) support
    This patch adds the initial skeleton for Three-wire UART (H5) support
    and hooks it up to the HCI UART framework.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- drivers/bluetooth/hci_ldisc.c
+++ drivers/bluetooth/hci_ldisc.c
@@ -722,6 +722,9 @@ static int __init hci_uart_init(void)
 #ifdef CONFIG_BT_HCIUART_ATH3K
 	ath_init();
 #endif
+#ifdef CONFIG_BT_HCIUART_3WIRE
+	h5_init();
+#endif
 
 	return 0;
 }
@@ -741,6 +744,9 @@ static void __exit hci_uart_exit(void)
 #endif
 #ifdef CONFIG_BT_HCIUART_ATH3K
 	ath_deinit();
+#endif
+#ifdef CONFIG_BT_HCIUART_3WIRE
+	h5_deinit();
 #endif
 
 	/* Release tty registration of line discipline */
