9d293c4f6bc170e0696422c58b30bcec7a6e25b4
David Rientjes <rientjes@google.com>
2012-07-31 16:43:40 -0700

    mm, oom: introduce helper function to process threads during scan
    This patch introduces a helper function to process each thread during the
    iteration over the tasklist.  A new return type, enum oom_scan_t, is
    defined to determine the future behavior of the iteration:
    
     - OOM_SCAN_OK: continue scanning the thread and find its badness,
    
     - OOM_SCAN_CONTINUE: do not consider this thread for oom kill, it's
       ineligible,
    
     - OOM_SCAN_ABORT: abort the iteration and return, or
    
     - OOM_SCAN_SELECT: always select this thread with the highest badness
       possible.
    
    There is no functional change with this patch.  This new helper function
    will be used in the next patch in the memory controller.
    
    Reviewed-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Acked-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Reviewed-by: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: David Rientjes <rientjes@google.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Sha Zhengju <handai.szj@taobao.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -52,10 +52,6 @@ extern void clear_zonelist_oom(struct zonelist *zonelist, gfp_t gfp_flags);
 extern void check_panic_on_oom(enum oom_constraint constraint, gfp_t gfp_mask,
 			       int order, const nodemask_t *nodemask);
 
-extern enum oom_scan_t oom_scan_process_thread(struct task_struct *task,
-		unsigned long totalpages, const nodemask_t *nodemask,
-		bool force_kill);
-
 extern void out_of_memory(struct zonelist *zonelist, gfp_t gfp_mask,
 		int order, nodemask_t *mask, bool force_kill);
 extern int register_oom_notifier(struct notifier_block *nb);
