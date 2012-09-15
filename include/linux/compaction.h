0231ac65ccaa253bdb02a8f9fbf75b91268455cf
Dorimanx <yuri@bynet.co.il>
2012-09-16 00:24:44 +0300

    Fixing/mixing and Max SYNC to compaction.c

--- include/linux/compaction.h
+++ include/linux/compaction.h
@@ -21,8 +21,8 @@ extern int sysctl_extfrag_handler(struct ctl_table *table, int write,
 
 extern int fragmentation_index(struct zone *zone, unsigned int order);
 extern unsigned long try_to_compact_pages(struct zonelist *zonelist,
-			int order, gfp_t gfp_mask, nodemask_t *mask,
-			bool sync);
+                        int order, gfp_t gfp_mask, nodemask_t *nodemask,
+                        bool sync, bool *contended);
 extern int compact_pgdat(pg_data_t *pgdat, int order);
 extern unsigned long compaction_suitable(struct zone *zone, int order);
 
@@ -101,7 +101,7 @@ extern void compaction_unregister_node(struct node *node);
 
 #else
 
-#if 0 //not used and declared in mm/compaction.c
+#if 0
 static inline int compaction_register_node(struct node *node)
 {
 	return 0;
