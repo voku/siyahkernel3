0c1e2996cc5d1c11a442ff05a01f823134b04a4d
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2012-10-20 14:53:12 +0300

    crypto: testmgr - add new larger DES3_EDE testvectors
    Most DES3_EDE testvectors are short and do not test parallelised codepaths
    well. Add larger testvectors to test large crypto operations and to test
    multi-page crypto with DES3_EDE.
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/tcrypt.c
+++ crypto/tcrypt.c
@@ -986,6 +986,7 @@ static int do_test(int m)
 	case 4:
 		ret += tcrypt_test("ecb(des3_ede)");
 		ret += tcrypt_test("cbc(des3_ede)");
+		ret += tcrypt_test("ctr(des3_ede)");
 		break;
 #endif
 #ifdef CONFIG_CRYPTO_MD4
