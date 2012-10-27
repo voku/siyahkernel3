1c7227b8a42ed9e8014f4981977dec62566e823c
Lars Moelleken <voku@voku-online.de>
2012-10-27 16:36:58 +0200

    + testing: updates from "android-samsung-3.0-jb-mr0" -> https://android.googlesource.com/kernel/samsung/+/android-samsung-3.0-jb-mr0 + testing: gcc-Version 4.7.2 (Debian 4.7.2-4) -> http://www.emdebian.org/debian/

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,6 +10,8 @@ export ARCH=arm
 export USE_SEC_FIPS_MODE=true
 
 # compiler
+# gcc local (gcc from http://www.emdebian.org/debian/)
+export CROSS_COMPILE=/usr/bin/arm-linux-gnueabi-
 # gcc 4.5.2
 #export CROSS_COMPILE=$PARENT_DIR/toolchain/bin/arm-none-eabi-
 # gcc 4.4.3 (CM9)
@@ -17,7 +19,7 @@ export USE_SEC_FIPS_MODE=true
 # gcc 4.7 (Linaro 12.04)
 # export CROSS_COMPILE=$PARENT_DIR/linaro/bin/arm-eabi-
 # gcc 4.7.2 (Linaro 12.07)
-export CROSS_COMPILE=$KERNELDIR/android-toolchain/bin/arm-eabi-
+#export CROSS_COMPILE=$KERNELDIR/android-toolchain/bin/arm-eabi-
 
 
 # Importing PATCH for GCC depend on GCC version.
