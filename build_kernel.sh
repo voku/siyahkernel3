673247b865bc2b28cce5394e6b267e485cc2e562
Lars Moelleken <voku@voku-online.de>
2012-12-09 00:43:53 +0100

    + linaro toolchain (released 2012-11-26)

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,26 +17,25 @@ export USE_SEC_FIPS_MODE=true
 # gcc 4.7 (Linaro 12.04)
 # export CROSS_COMPILE=$PARENT_DIR/linaro/bin/arm-eabi-
 # gcc 4.7.2 (Linaro 12.07)
-export CROSS_COMPILE=$KERNELDIR/android-toolchain/bin/arm-eabi-
+export CROSS_COMPILE=$KERNELDIR/android-toolchain/bin/arm-linux-gnueabihf-
 
 
 # Importing PATCH for GCC depend on GCC version.
-GCCVERSION_OLD=`${CROSS_COMPILE}gcc --version | cut -d " " -f3 | cut -c3-5 | grep -iv "09" | grep -iv "ee" | grep -iv "en"`
-GCCVERSION_NEW=`${CROSS_COMPILE}gcc --version | cut -d " " -f4 | cut -c1-3 | grep -iv "Fre" | grep -iv "sof" | grep -iv "for" | grep -iv "auc"`
+GCCVERSION=`./scripts/gcc-version.sh ${CROSS_COMPILE}gcc`
 
-if [ "a$GCCVERSION_OLD" == "a4.3" ]; then
+if [ "a$GCCVERSION" == "a0404" ]; then
 	cp $KERNELDIR/arch/arm/boot/compressed/Makefile_old_gcc $KERNELDIR/arch/arm/boot/compressed/Makefile
 	echo "GCC 4.3.X Compiler Detected, building"
-elif [ "a$GCCVERSION_OLD" == "a4.4" ]; then
+elif [ "a$GCCVERSION" == "a0404" ]; then
 	cp $KERNELDIR/arch/arm/boot/compressed/Makefile_old_gcc $KERNELDIR/arch/arm/boot/compressed/Makefile
 	echo "GCC 4.4.X Compiler Detected, building"
-elif [ "a$GCCVERSION_OLD" == "a4.5" ]; then
+elif [ "a$GCCVERSION" == "a0405" ]; then
 	cp $KERNELDIR/arch/arm/boot/compressed/Makefile_old_gcc $KERNELDIR/arch/arm/boot/compressed/Makefile
 	echo "GCC 4.5.X Compiler Detected, building"
-elif [ "a$GCCVERSION_NEW" == "a4.6" ]; then
+elif [ "a$GCCVERSION" == "a0406" ]; then
 	cp $KERNELDIR/arch/arm/boot/compressed/Makefile_linaro $KERNELDIR/arch/arm/boot/compressed/Makefile
 	echo "GCC 4.6.X Compiler Detected, building"
-elif [ "a$GCCVERSION_NEW" == "a4.7" ]; then
+elif [ "a$GCCVERSION" == "a0407" ]; then
 	cp $KERNELDIR/arch/arm/boot/compressed/Makefile_linaro $KERNELDIR/arch/arm/boot/compressed/Makefile
 	echo "GCC 4.7.X Compiler Detected, building"
 else
