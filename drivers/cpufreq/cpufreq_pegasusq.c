8134349a16870e3bbc335c600e0bea32f92a94f4
Lars Moelleken <voku@voku-online.de>
2012-12-29 23:35:47 +0100

    + "Pegasusq tweaks primarily suggested by mnagy1987" - https://github.com/AndroidDeveloperAlliance/ZenKernel_TUNA/commit/47630c49926caf57d6ebcd32b0f90b93d8c4162b

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -147,10 +147,10 @@ static unsigned int get_nr_run_avg(void)
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_SAMPLING_DOWN_FACTOR		(2)
+#define DEF_SAMPLING_DOWN_FACTOR		(3)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
-#define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(5)
-#define DEF_FREQUENCY_UP_THRESHOLD		(85)
+#define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(13)
+#define DEF_FREQUENCY_UP_THRESHOLD		(97)
 
 /* for multiple freq_step */
 #define DEF_UP_THRESHOLD_DIFF	(5)
@@ -158,8 +158,8 @@ static unsigned int get_nr_run_avg(void)
 #define DEF_FREQUENCY_MIN_SAMPLE_RATE		(10000)
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
-#define DEF_SAMPLING_RATE			(50000)
-#define MIN_SAMPLING_RATE			(10000)
+#define DEF_SAMPLING_RATE			(65000)
+#define MIN_SAMPLING_RATE			(20000)
 #define MAX_HOTPLUG_RATE			(40u)
 
 #define DEF_MAX_CPU_LOCK			(0)
@@ -167,16 +167,16 @@ static unsigned int get_nr_run_avg(void)
 #define DEF_CPU_UP_FREQ				(500000)
 #define DEF_CPU_DOWN_FREQ			(200000)
 #define DEF_UP_NR_CPUS				(1)
-#define DEF_CPU_UP_RATE				(10)
-#define DEF_CPU_DOWN_RATE			(20)
-#define DEF_FREQ_STEP				(37)
+#define DEF_CPU_UP_RATE				(7)
+#define DEF_CPU_DOWN_RATE			(3)
+#define DEF_FREQ_STEP				(20)
 /* for multiple freq_step */
 #define DEF_FREQ_STEP_DEC			(13)
 
 #define DEF_START_DELAY				(0)
 
-#define UP_THRESHOLD_AT_MIN_FREQ		(40)
-#define FREQ_FOR_RESPONSIVENESS			(400000)
+#define UP_THRESHOLD_AT_MIN_FREQ		(60)
+#define FREQ_FOR_RESPONSIVENESS			(300000)
 /* for fast decrease */
 #define FREQ_FOR_FAST_DOWN				(1200000)
 #define UP_THRESHOLD_AT_FAST_DOWN		(95)
@@ -1455,6 +1455,8 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 		dbs_tuners_ins.max_freq = policy->max;
 		dbs_tuners_ins.min_freq = policy->min;
+		dbs_tuners_ins.cpu_down_freq = policy->min;
+
 		hotplug_history->num_hist = 0;
 		start_rq_work();
 
@@ -1488,7 +1490,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 			min_sampling_rate = MIN_SAMPLING_RATE;
 			dbs_tuners_ins.sampling_rate = DEF_SAMPLING_RATE;
-			dbs_tuners_ins.io_is_busy = 0;
+			dbs_tuners_ins.io_is_busy = 1;
 		}
 		mutex_unlock(&dbs_mutex);
 
