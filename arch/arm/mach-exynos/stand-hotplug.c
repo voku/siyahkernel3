4e0574ccd69c9493347e165c4bf3b5ff748fc3fc
Yuri.Sh <yuri@bynet.co.il>
2012-12-30 02:42:37 +0200

    Removed ZenX and nightmage GOVS from allowed hot plug control.
    ZenX cant boot second CPU! one one active. if allowed to use hotplug
    nightmare stuck kernel if allowed to use hotplug.
    
    without hotplug control, all OK. and working good.

--- arch/arm/mach-exynos/stand-hotplug.c
+++ arch/arm/mach-exynos/stand-hotplug.c
@@ -630,8 +630,6 @@ static int standhotplug_cpufreq_policy_notifier_call(struct notifier_block *this
 			(!strnicmp(policy->governor->name, "pegasusq", CPUFREQ_NAME_LEN)) ||
 			(!strnicmp(policy->governor->name, "lulzactiveq", CPUFREQ_NAME_LEN)) ||
 			(!strnicmp(policy->governor->name, "abyssplug", CPUFREQ_NAME_LEN)) ||
-			(!strnicmp(policy->governor->name, "ZenX", CPUFREQ_NAME_LEN)) ||
-			(!strnicmp(policy->governor->name, "nightmare", CPUFREQ_NAME_LEN)) ||
 			(!strnicmp(policy->governor->name, "hotplug", CPUFREQ_NAME_LEN))
 			)
 		{
@@ -671,8 +669,6 @@ static int __init exynos4_pm_hotplug_device_init(void)
 
 #if defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ) || \
 	defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG) || \
-	defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE) || \
-	defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZENERACTIVE) || \
 	defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG) || \
 	defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEQ)
 	standhotplug_enabled = 0;
