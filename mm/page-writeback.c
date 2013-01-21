c6275a778a4395b926eb72c4c2d97addd493501f
Lars Moelleken <voku@voku-online.de>
2013-01-21 22:08:27 +0100

    + page-writeback.c: testing ...

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -1638,7 +1638,7 @@ static struct notifier_block __cpuinitdata ratelimit_nb = {
 
 static void dirty_early_suspend(struct early_suspend *handler)
 {
-	dirty_writeback_interval = 5 * 100;
+	dirty_writeback_interval = 5 * 200;
 }
 
 static void dirty_late_resume(struct early_suspend *handler)
