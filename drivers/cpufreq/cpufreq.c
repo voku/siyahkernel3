0534b4dd92e3fc7974ac0e587db4ae2a008af506
Dirk Brandewie <dirk.brandewie@gmail.com>
2013-02-06 09:02:11 -0800

    cpufreq: Do not track governor name for scaling drivers with internal governors.
    Scaling drivers that implement internal governors do not have governor
    structures assocaited with them.  Only track the name of the governor
    associated with the CPU if the driver does not implement
    cpufreq_driver.setpolicy()
    
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1090,8 +1090,9 @@ static int __cpufreq_remove_dev(struct device *dev, struct subsys_interface *sif
 #endif
 
 #ifdef CONFIG_HOTPLUG_CPU
-	strncpy(per_cpu(cpufreq_cpu_governor, cpu), data->governor->name,
-			CPUFREQ_NAME_LEN);
+	if (!cpufreq_driver->setpolicy)
+		strncpy(per_cpu(cpufreq_cpu_governor, cpu),
+			data->governor->name, CPUFREQ_NAME_LEN);
 #endif
 
 	WARN_ON(lock_policy_rwsem_write(cpu));
