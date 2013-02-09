9294e774eb7d87549a223f3ee2ab83f9ebcd5d4d
Lars Moelleken <voku@voku-online.de>
2013-02-09 18:11:25 +0100

    + sync "drivers/battery/" & "drivers/power/*charger*" with HK_JB (sgs2) kernel from samsung

--- drivers/power/sec_battery_u1.c
+++ drivers/power/sec_battery_u1.c
@@ -28,7 +28,6 @@
 #include <linux/android_alarm.h>
 #include <plat/adc.h>
 #include <linux/power/sec_battery_u1.h>
-#include "charge_current.h"
 
 #if defined(CONFIG_TARGET_LOCALE_NA) || defined(CONFIG_TARGET_LOCALE_NAATT) || \
 	defined(CONFIG_TARGET_LOCALE_NAATT_TEMP)
@@ -331,7 +330,6 @@ struct sec_bat_info {
 	unsigned int batt_temp_radc;
 #endif
 	unsigned int batt_current_adc;
-	int batt_chg_current;
 #if defined(CONFIG_TARGET_LOCALE_NAATT) || \
 	defined(CONFIG_TARGET_LOCALE_NAATT_TEMP)
 	int batt_vf_adc;
@@ -668,14 +666,6 @@ static int sec_bat_set_property(struct power_supply *ps,
 		/* cable is attached or detached. called by USB switch(MUIC) */
 		dev_info(info->dev, "%s: cable was changed(%d)\n", __func__,
 			 val->intval);
-
-#ifdef CONFIG_KEYBOARD_CYPRESS_AOKP
-		/* trigger cypress bln */
-		enable_bln_charging(val->intval);
-#endif
-		/* trigger touchscreen config update */
-		tsp_touch_config_update(val->intval);
-
 		switch (val->intval) {
 		case POWER_SUPPLY_TYPE_BATTERY:
 			info->cable_type = CABLE_TYPE_NONE;
@@ -830,13 +820,13 @@ static int is_event_end_timer_running(struct sec_bat_info *info)
 
 	if (time_after(passed_time, (unsigned long)EVENT_OVER_TIME)) {
 		info->event_end_time = 0xFFFFFFFF;
-		#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+		#ifndef PRODUCT_SHIP
 		dev_info(info->dev, "%s: Event timer is over 10 min\n",
 			 __func__);
 		#endif
 		return false;
 	} else {
-	#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+	#ifndef PRODUCT_SHIP
 		dev_info(info->dev, "%s: Event timer is running(%u s)\n",
 			 __func__, jiffies_to_msecs(passed_time) / 1000);
 	#endif
@@ -867,13 +857,13 @@ static int is_event_end_timer_running(struct sec_bat_info *info)
 
 	if (time_after(passed_time, (unsigned long)BAT_USE_TIMER_EXPIRE)) {
 		info->event_expired_time = 0xFFFFFFFF;
-		#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+		#ifndef PRODUCT_SHIP
 		dev_info(info->dev, "[SPR_NA] %s: Event timer is over 10 min\n",
 			 __func__);
 		#endif
 		return false;
 	} else {
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 		dev_info(info->dev,
 			 "[SPR_NA] %s: Event timer is running(%u s)\n",
 			 __func__, jiffies_to_msecs(passed_time) / 1000);
@@ -987,7 +977,7 @@ static int sec_bat_check_temper(struct sec_bat_info *info)
 		} else {
 			if ((info->batt_event_status)
 			    || (is_event_end_timer_running(info))) {
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 				dev_info(info->dev,
 					 "%s: [NA_SPR] Changed Put off Current",
 					 __func__);
@@ -1130,7 +1120,7 @@ static int sec_bat_check_temper(struct sec_bat_info *info)
 				__func__, ret);
 		}
 	}
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 	dev_info(info->dev, "%s: temp=%d, adc=%d\n", __func__, temp, temp_adc);
 #endif
 	return temp;
@@ -1322,7 +1312,7 @@ static int sec_bat_check_temper(struct sec_bat_info *info)
 				__func__, ret);
 		}
 	}
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 	dev_info(info->dev, "%s: temp=%d, adc=%d\n", __func__, temp, temp_adc);
 #endif
 
@@ -1410,7 +1400,7 @@ static int sec_bat_check_temper(struct sec_bat_info *info)
 				__func__, ret);
 		}
 	}
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 	dev_info(info->dev, "%s: temp=%d, adc=%d\n", __func__, temp, temp_adc);
 #endif
 	return temp;
@@ -1506,15 +1496,15 @@ static int sec_bat_enable_charging_main(struct sec_bat_info *info, bool enable)
 		switch (info->cable_type) {
 		case CABLE_TYPE_USB:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = charge_current_usb;	/* mA */
+			val_chg_current.intval = 450;	/* mA */
 			break;
 		case CABLE_TYPE_AC:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = charge_current_ac;	/* mA */
+			val_chg_current.intval = 650;	/* mA */
 			break;
 		case CABLE_TYPE_MISC:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = charge_current_misc;	/* mA */
+			val_chg_current.intval = 450;	/* mA */
 			break;
 		default:
 			dev_err(info->dev, "%s: Invalid func use\n", __func__);
@@ -1595,7 +1585,7 @@ static int sec_bat_enable_charging_sub(struct sec_bat_info *info, bool enable)
 			switch (info->cable_type) {
 			case CABLE_TYPE_USB:
 				val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-				val_chg_current.intval = charge_current_usb;	/* mA */
+				val_chg_current.intval = 450;	/* mA */
 				break;
 			case CABLE_TYPE_AC:
 				val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
@@ -1606,11 +1596,11 @@ static int sec_bat_enable_charging_sub(struct sec_bat_info *info, bool enable)
 					val_chg_current.intval = 450;	/* mA */
 				else
 #endif
-			val_chg_current.intval = charge_current_ac;	/* mA */
+					val_chg_current.intval = 650;	/* mA */
 				break;
 			case CABLE_TYPE_MISC:
 				val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-				val_chg_current.intval = charge_current_misc;	/* mA */
+				val_chg_current.intval = 450;	/* mA */
 				break;
 			default:
 				dev_err(info->dev, "%s: Invalid func use\n",
@@ -1791,7 +1781,7 @@ static bool sec_bat_charging_time_management(struct sec_bat_info *info)
 		dev_info(info->dev, "%s: Undefine Battery Status\n", __func__);
 		return false;
 	}
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 	dev_info(info->dev, "Time past : %u secs\n",
 		 jiffies_to_msecs(info->charging_passed_time) / 1000);
 #endif
@@ -1899,7 +1889,7 @@ static void sec_bat_check_vf(struct sec_bat_info *info)
 		info->present_count = 0;
 	}
 
-	dev_dbg(info->dev, "%s: Battery Health (%d)\n",
+	dev_info(info->dev, "%s: Battery Health (%d)\n",
 		 __func__, info->batt_health);
 	return;
 }
@@ -2140,23 +2130,10 @@ static bool sec_bat_check_ing_level_trigger(struct sec_bat_info *info)
 	}
 }
 
-#ifdef CONFIG_KEYBOARD_CYPRESS_AOKP
-unsigned int batt_status;
-unsigned int charging_status;
-#endif
-
 static void sec_bat_monitor_work(struct work_struct *work)
 {
 	struct sec_bat_info *info = container_of(work, struct sec_bat_info,
 						 monitor_work);
-#ifdef CONFIG_KEYBOARD_CYPRESS_AOKP
-	/* Broadcast battery level */
-	batt_status = info->batt_soc;
-
-	/* Broadcast charging status */
-	charging_status = info->charging_status;
-#endif
-
 
 	sec_bat_check_temper(info);
 #ifndef SEC_BATTERY_INDEPEDENT_VF_CHECK
@@ -2310,7 +2287,7 @@ static void sec_bat_monitor_work(struct work_struct *work)
 		 info->batt_temp / 10, info->charging_status, info->batt_health,
 		 info->batt_vf_adc);
 #else
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 	dev_info(info->dev,
 		 "soc(%d), vfocv(%d), vcell(%d), temp(%d), charging(%d), health(%d), chg_adc(%d)\n",
 		 info->batt_soc, info->batt_vfocv, info->batt_vcell / 1000,
@@ -2376,20 +2353,6 @@ static void sec_bat_polling_work(struct work_struct *work)
 				      msecs_to_jiffies(info->polling_interval));
 }
 
-
-int sec_bat_check_chgcurrent(struct sec_bat_info *info)
-{
-	unsigned long cadc = 0;
-
-	mutex_lock(&info->adclock);
-	cadc = sec_bat_get_adc_data(info, ADC_CH_CHGCURRENT);
-	mutex_unlock(&info->adclock);
-	if(cadc<0) info->batt_chg_current=cadc; else
-	//fit & normalize - gm
-	info->batt_chg_current = (cadc*50-(cadc*cadc/10000*84))/100;
-	return info->batt_chg_current;
-}
-
 #define SEC_BATTERY_ATTR(_name)			\
 {						\
 	.attr = { .name = #_name,		\
@@ -2419,7 +2382,6 @@ static struct device_attribute sec_battery_attrs[] = {
 	SEC_BATTERY_ATTR(batt_test_value),
 	SEC_BATTERY_ATTR(batt_current_now),
 	SEC_BATTERY_ATTR(batt_current_adc),
-	SEC_BATTERY_ATTR(batt_chg_current),
 	SEC_BATTERY_ATTR(siop_activated),
 	SEC_BATTERY_ATTR(system_rev),
 #ifdef CONFIG_TARGET_LOCALE_NA
@@ -2475,7 +2437,6 @@ enum {
 	BATT_TEST_VALUE,
 	BATT_CURRENT_NOW,
 	BATT_CURRENT_ADC,
-	BATT_CHG_CURRENT,
 	BATT_SIOP_ACTIVATED,
 	BATT_SYSTEM_REV,
 	BATT_FG_PSOC,
@@ -2551,7 +2512,7 @@ static void sec_bat_check_event_status(struct sec_bat_info *info, int mode,
 		if (info->batt_event_status & offset)
 			info->batt_event_status &= ~offset;
 	}
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
+#ifndef PRODUCT_SHIP
 	printk(KERN_DEBUG "[%s] current batt_event_status = 0x%x\n", __func__,
 	       info->batt_event_status);
 #endif
@@ -2695,12 +2656,6 @@ static ssize_t sec_bat_show_property(struct device *dev,
 	case BATT_CURRENT_ADC:
 		i += scnprintf(buf + i, PAGE_SIZE - i, "%d\n",
 			       info->batt_current_adc);
-		break;
-	case BATT_CHG_CURRENT:
-		if (info->charging_status != POWER_SUPPLY_STATUS_DISCHARGING) {
-			val = sec_bat_check_chgcurrent(info);
-			i += scnprintf(buf + i, PAGE_SIZE - i, "%d\n", val);
-		} else i = -EINVAL;
 		break;
 	case BATT_SYSTEM_REV:
 		i += scnprintf(buf + i, PAGE_SIZE - i, "%d\n", system_rev);
@@ -3079,7 +3034,6 @@ static int sec_bat_create_attrs(struct device *dev)
 	while (i--)
 		device_remove_file(dev, &sec_battery_attrs[i]);
  succeed:
-	charge_current_start();
 	return rc;
 }
 
@@ -3124,7 +3078,6 @@ static int sec_bat_read_proc(char *buf, char **start,
 		      info->batt_vfocv,
 		      info->batt_vcell,
 		      info->batt_current_adc,
-		      info->batt_chg_current,
 		      info->batt_full_status,
 		      info->charging_int_full_count,
 		      info->charging_adc_full_count,
