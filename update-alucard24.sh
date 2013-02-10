d4ef56598383fdc9270ef33ede2dd7b9bf3184be
Yuri.Sh <yuri@bynet.co.il>
2013-02-10 16:49:43 +0200

    D-Team updates scripts mod.

--- update-alucard24.sh
+++ update-alucard24.sh
@@ -1,7 +1,5 @@
-#git fetch https://github.com/Alucard24/Dorimanx-SG2-I9100-Kernel.git
 git fetch git://github.com/Alucard24/Dorimanx-SG2-I9100-Kernel.git
 cd ../initramfs3/
-#git fetch https://github.com/Alucard24/initramfs3.git
 git fetch git://github.com/Alucard24/initramfs3.git
 cd ../dorimanx-SG2-I9100-Kernel
 
