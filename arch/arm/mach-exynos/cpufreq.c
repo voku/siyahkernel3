e68b0e9e36cf3e0b96a7c2cb649a2fd7291b8b5a
Yuri.Sh <yuri@bynet.co.il>
2013-02-22 14:14:01 +0200

    Removed 1052Mhz and 1152Mhz Freqs from system. now we have from 100 to 1.0Ghz by 50Mhz and from 1.0Ghz to 1.6Ghz by 100Mhz.

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -91,7 +91,7 @@ static unsigned int exynos_get_safe_armvolt(unsigned int old_index, unsigned int
 	return safe_arm_volt;
 }
 
-unsigned int smooth_level = L12; /* L12 = 800Mhz */
+unsigned int smooth_level = L10; /* L10 = 800Mhz */
 
 static int exynos_target(struct cpufreq_policy *policy,
 			  unsigned int target_freq,
@@ -140,8 +140,8 @@ static int exynos_target(struct cpufreq_policy *policy,
 
 #if defined(CONFIG_CPU_EXYNOS4210)
 	/* Do NOT step up max arm clock directly to reduce power consumption */
-	if (index <= 4 && old_index > smooth_level && smooth_level >= L8)
-		index = smooth_level; /* L8 = 1000Mhz */
+	if (index <= 4 && old_index > smooth_level && smooth_level >= L6)
+		index = smooth_level; /* L6 = 1000Mhz */
 #endif
 
 	freqs.new = freq_table[index].frequency;
