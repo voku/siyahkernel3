1e56dfa273d36ee8359e405ec97de8b3863e6558
Lars Moelleken <voku@voku-online.de>
2013-03-08 01:27:30 +0100

    + mm-drop_swap_cache_aggressively -> http://ck.kolivas.org/patches/3.0/3.8/3.8-ck1/patches/mm-drop_swap_cache_aggressively.patch

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -359,7 +359,7 @@ extern struct page *swapin_readahead(swp_entry_t, gfp_t,
 extern atomic_long_t nr_swap_pages;
 extern long total_swap_pages;
 
-/* Swap 50% full? Release swapcache more aggressively.. */
+/* Swap 50% full? */
 static inline bool vm_swap_full(void)
 {
 	return atomic_long_read(&nr_swap_pages) * 2 < total_swap_pages;
