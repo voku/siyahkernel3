aa60018ba407e52f311b1e0861bb3d49bae6e43d
Thomas Gleixner <tglx@linutronix.de>
2013-03-21 22:49:35 +0100

    idle: Implement generic idle function
    All idle functions in arch/* are more or less the same, plus minus a
    few bugs and extra instrumentation, tickless support and other
    optional items.
    
    Implement a generic idle function which resembles the functionality
    found in arch/. Provide weak arch_cpu_idle_* functions which can be
    overridden by the architecture code if needed.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Paul McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Reviewed-by: Cc: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Cc: Magnus Damm <magnus.damm@gmail.com>
    Link: http://lkml.kernel.org/r/20130321215233.646635455@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Conflicts:
    	arch/Kconfig
    	include/linux/cpu.h

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -220,11 +220,12 @@ enum cpuhp_state {
 void cpu_startup_entry(enum cpuhp_state state);
 void cpu_idle(void);
 
-#define IDLE_START 1
-#define IDLE_END 2
+void cpu_idle_poll_ctrl(bool enable);
 
-void idle_notifier_register(struct notifier_block *n);
-void idle_notifier_unregister(struct notifier_block *n);
-void idle_notifier_call_chain(unsigned long val);
+void arch_cpu_idle(void);
+void arch_cpu_idle_prepare(void);
+void arch_cpu_idle_enter(void);
+void arch_cpu_idle_exit(void);
+void arch_cpu_idle_dead(void);
 
 #endif /* _LINUX_CPU_H_ */
