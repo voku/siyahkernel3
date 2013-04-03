7e274a7666dd2ef300449fe5f2dc7614c0f3b231
Yuri.Sh <yuri@bynet.co.il>
2013-04-03 19:13:34 +0300

    Small Fix in build name.

--- build_kernel.sh
+++ build_kernel.sh
@@ -166,7 +166,7 @@ if [ -e ${KERNELDIR}/arch/arm/boot/zImage ]; then
 	rm ${KERNELDIR}/READY-JB/boot/zImage
 	rm ${KERNELDIR}/READY-JB/Kernel_*
 	stat ${KERNELDIR}/zImage
-	GETVER=`grep 'Siyah-.*-V' arch/arm/configs/${KERNEL_CONFIG} | sed 's/.*".//g' | cut -c 1-20`
+	GETVER=`grep 'Siyah-.*-V' arch/arm/configs/${KERNEL_CONFIG} | sed 's/.*".//g' | sed 's/-J.*//g'`
 	cp ${KERNELDIR}/zImage /${KERNELDIR}/READY-JB/boot/
 	cd ${KERNELDIR}/READY-JB/
 	zip -r Kernel_${GETVER}-`date +"[%H-%M]-[%d-%m]-JB-CM-AOKP-SGII-PWR-CORE"`.zip .
@@ -174,7 +174,7 @@ if [ -e ${KERNELDIR}/arch/arm/boot/zImage ]; then
 	if [ "$STATUS" == "device" ]; then
 		read -p "push kernel to android (y/n)?"
 		if [ "$REPLY" == "y" ]; then
-			adb push ${KERNELDIR}/READY-JB/Kernel_*ICS-JB*.zip /sdcard/;
+			adb push ${KERNELDIR}/READY-JB/Kernel_*JB*.zip /sdcard/;
 		fi;
 	fi;
 else
