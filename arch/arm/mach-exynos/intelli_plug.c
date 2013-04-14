531a0c5ad83fd4b26db041a28c4962998f8c13c7
Lars Moelleken <voku@voku-online.de>
2013-04-14 03:24:03 +0200

    + testing v2 for "intelli_plug.c"

--- arch/arm/mach-exynos/intelli_plug.c
+++ arch/arm/mach-exynos/intelli_plug.c
@@ -20,6 +20,7 @@
 #include <linux/sched.h>
 #include <linux/mutex.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 
 #define INTELLI_PLUG_MAJOR_VERSION	1
 #define INTELLI_PLUG_MINOR_VERSION	5
@@ -28,8 +29,6 @@
 #define DEF_SAMPLING_MS			(200)
 
 #define DUAL_CORE_PERSISTENCE		15
-#define TRI_CORE_PERSISTENCE		12
-#define QUAD_CORE_PERSISTENCE		10
 
 #define RUN_QUEUE_THRESHOLD		38
 
@@ -39,7 +38,7 @@ static DEFINE_MUTEX(intelli_plug_mutex);
 
 struct delayed_work intelli_plug_work;
 
-static unsigned int intelli_plug_active = 0;
+static unsigned int intelli_plug_active = 1;
 module_param(intelli_plug_active, uint, 0644);
 
 static unsigned int eco_mode_active = 0;
@@ -53,80 +52,134 @@ static unsigned int nr_fshift = NR_FSHIFT;
 module_param(nr_fshift, uint, 0644);
 
 static unsigned int nr_run_thresholds_full[] = {
-/* 	1,  2,  3,  4 - on-line cpus target */
-	5,  7,  9,  UINT_MAX /* avg run threads * 2 (e.g., 9 = 2.25 threads) */
-	};
+/* 	1,  2 - on-line cpus target */
+	5,  UINT_MAX /* avg run threads * 2 (e.g., 9 = 2.25 threads) */
+};
 
 static unsigned int nr_run_thresholds_eco[] = {
-/*      1,  2, - on-line cpus target */
-        3,  UINT_MAX /* avg run threads * 2 (e.g., 9 = 2.25 threads) */
-        };
+/*  1,  2 - on-line cpus target */
+    3,  UINT_MAX /* avg run threads * 2 (e.g., 9 = 2.25 threads) */
+};
 
 static unsigned int nr_run_hysteresis = 4;  /* 0.5 thread */
 module_param(nr_run_hysteresis, uint, 0644);
 
-static unsigned int nr_run_last;
+#ifndef CONFIG_CPU_EXYNOS4210
+#define RQ_AVG_TIMER_RATE	10
+#else
+#define RQ_AVG_TIMER_RATE	20
+#endif
 
-static unsigned int NwNs_Threshold[] = { 19, 30,  19,  11,  19,  11, 0,  11};
-static unsigned int TwTs_Threshold[] = {140,  0, 140, 190, 140, 190, 0, 190};
+struct runqueue_data {
+	unsigned int nr_run_avg;
+	unsigned int update_rate;
+	int64_t last_time;
+	int64_t total_time;
+	struct delayed_work work;
+	struct workqueue_struct *nr_run_wq;
+	spinlock_t lock;
+};
 
-static int mp_decision(void)
+static struct runqueue_data *rq_data;
+static void rq_work_fn(struct work_struct *work);
+
+static void start_rq_work(void)
 {
-	static bool first_call = true;
-	int new_state = 0;
-	int nr_cpu_online;
-	int index;
-	static cputime64_t total_time = 0;
-	static cputime64_t last_time;
-	cputime64_t current_time;
-	cputime64_t this_time = 0;
+	rq_data->nr_run_avg = 0;
+	rq_data->last_time = 0;
+	rq_data->total_time = 0;
+	if (rq_data->nr_run_wq == NULL)
+		rq_data->nr_run_wq =
+			create_singlethread_workqueue("nr_run_avg");
 
-	current_time = ktime_to_ms(ktime_get());
-	if (first_call) {
-		first_call = false;
-	} else {
-		this_time = current_time - last_time;
-	}
-	total_time += this_time;
-
-	nr_cpu_online = num_online_cpus();
-
-	if (nr_cpu_online) {
-		index = (nr_cpu_online - 1) * 2;
-		if (nr_cpu_online < 2) {
-			if (total_time >= TwTs_Threshold[index]) {
-				new_state = 1;
-			}
-		} if (total_time >= TwTs_Threshold[index+1] ) {
-			new_state = 0;
-		} else {
-			total_time = 0;
-		}
-	} else {
-		total_time = 0;
-	}
-
-	last_time = ktime_to_ms(ktime_get());
-
-	return new_state;
+	queue_delayed_work(rq_data->nr_run_wq, &rq_data->work,
+			   msecs_to_jiffies(rq_data->update_rate));
+	return;
 }
+
+static void stop_rq_work(void)
+{
+	if (rq_data->nr_run_wq)
+		cancel_delayed_work(&rq_data->work);
+	return;
+}
+
+static int __init init_rq_avg(void)
+{
+	rq_data = kzalloc(sizeof(struct runqueue_data), GFP_KERNEL);
+	if (rq_data == NULL) {
+		pr_err("%s cannot allocate memory\n", __func__);
+		return -ENOMEM;
+	}
+	spin_lock_init(&rq_data->lock);
+	rq_data->update_rate = RQ_AVG_TIMER_RATE;
+	INIT_DEFERRABLE_WORK(&rq_data->work, rq_work_fn);
+
+	return 0;
+}
+
+static void rq_work_fn(struct work_struct *work)
+{
+	int64_t time_diff = 0;
+	int64_t nr_run = 0;
+	unsigned long flags = 0;
+	int64_t cur_time = ktime_to_ns(ktime_get());
+
+	spin_lock_irqsave(&rq_data->lock, flags);
+
+	if (rq_data->last_time == 0)
+		rq_data->last_time = cur_time;
+	if (rq_data->nr_run_avg == 0)
+		rq_data->total_time = 0;
+
+	nr_run = nr_running() * 100;
+	time_diff = cur_time - rq_data->last_time;
+	do_div(time_diff, 1000 * 1000);
+
+	if (time_diff != 0 && rq_data->total_time != 0) {
+		nr_run = (nr_run * time_diff) +
+			(rq_data->nr_run_avg * rq_data->total_time);
+		do_div(nr_run, rq_data->total_time + time_diff);
+	}
+	rq_data->nr_run_avg = nr_run;
+	rq_data->total_time += time_diff;
+	rq_data->last_time = cur_time;
+
+	if (rq_data->update_rate != 0)
+		queue_delayed_work(rq_data->nr_run_wq, &rq_data->work,
+				   msecs_to_jiffies(rq_data->update_rate));
+
+	spin_unlock_irqrestore(&rq_data->lock, flags);
+}
+
+static unsigned int get_nr_run_avg(void)
+{
+	unsigned int nr_run_avg;
+	unsigned long flags = 0;
+
+	spin_lock_irqsave(&rq_data->lock, flags);
+	nr_run_avg = rq_data->nr_run_avg;
+	rq_data->nr_run_avg = 0;
+	spin_unlock_irqrestore(&rq_data->lock, flags);
+
+	return nr_run_avg;
+}
+
+static unsigned int nr_run_last;
 
 static unsigned int calculate_thread_stats(void)
 {
+	unsigned int avg_nr_run = get_nr_run_avg();
 	unsigned int nr_run;
 	unsigned int threshold_size;
 
 	if (!eco_mode_active) {
 		threshold_size =  ARRAY_SIZE(nr_run_thresholds_full);
 		nr_run_hysteresis = 8;
-		nr_fshift = 3;
-		//pr_info("intelliplug: full mode active!");
 	}
 	else {
 		threshold_size =  ARRAY_SIZE(nr_run_thresholds_eco);
 		nr_run_hysteresis = 4;
-		nr_fshift = 1;
-		//pr_info("intelliplug: eco mode active!");
 	}
 
 	for (nr_run = 1; nr_run < threshold_size; nr_run++) {
@@ -138,6 +191,9 @@ static unsigned int calculate_thread_stats(void)
 
 		if (nr_run_last <= nr_run)
 			nr_threshold += nr_run_hysteresis;
+
+		if (avg_nr_run <= (nr_threshold << (FSHIFT - nr_fshift)))
+			break;
 	}
 	nr_run_last = nr_run;
 
@@ -147,83 +203,30 @@ static unsigned int calculate_thread_stats(void)
 static void __cpuinit intelli_plug_work_fn(struct work_struct *work)
 {
 	unsigned int nr_run_stat;
-	unsigned int cpu_count = 0;
-	unsigned int nr_cpus = 0;
-
-	int decision = 0;
 
 	if (intelli_plug_active == 1) {
 		nr_run_stat = calculate_thread_stats();
 
-		cpu_count = nr_run_stat;
-		// detect artificial loads or constant loads
-		// using msm rqstats
-		nr_cpus = num_online_cpus();
-		if (!eco_mode_active && (nr_cpus >= 1 && nr_cpus < 4)) {
-			decision = mp_decision();
-			if (decision) {
-				switch (nr_cpus) {
-					case 2:
-						cpu_count = 3;
-						//pr_info("nr_run(2) => %u\n", nr_run_stat);
-						break;
-					case 3:
-						cpu_count = 4;
-						//pr_info("nr_run(3) => %u\n", nr_run_stat);
-						break;
-				}
-			}
-		}
-
 		if (!suspended) {
-			switch (cpu_count) {
+			switch (nr_run_stat) {
 				case 1:
-					if (persist_count > 0)
+					if (persist_count > 0) {
 						persist_count--;
-					if (nr_cpus == 2 && persist_count == 0)
-						cpu_down(1);
-					if (eco_mode_active) {
-						cpu_down(3);
-						cpu_down(2);
 					}
-					//pr_info("case 1: %u\n", persist_count);
+					else if (num_online_cpus() == 2) {
+						cpu_down(1);
+					}
 					break;
 				case 2:
-					persist_count = DUAL_CORE_PERSISTENCE;
-					if (!decision)
-						persist_count = DUAL_CORE_PERSISTENCE / CPU_DOWN_FACTOR;
-					if (nr_cpus == 1)
+					persist_count = DUAL_CORE_PERSISTENCE / CPU_DOWN_FACTOR;
+					if (num_online_cpus() == 1)
 						cpu_up(1);
-					else {
-						cpu_down(3);
-						cpu_down(2);
-					}
-					//pr_info("case 2: %u\n", persist_count);
-					break;
-				case 3:
-					persist_count = TRI_CORE_PERSISTENCE;
-					if (!decision)	
-						persist_count = TRI_CORE_PERSISTENCE / CPU_DOWN_FACTOR;
-					if (nr_cpus == 2)
-						cpu_up(2);
-					else
-						cpu_down(3);
-					//pr_info("case 3: %u\n", persist_count);
-					break;
-				case 4:
-					persist_count = QUAD_CORE_PERSISTENCE;
-					if (!decision)	
-						persist_count = QUAD_CORE_PERSISTENCE / CPU_DOWN_FACTOR;
-					if (nr_cpus == 3)
-						cpu_up(3);
-					//pr_info("case 4: %u\n", persist_count);
 					break;
 				default:
 					pr_err("Run Stat Error: Bad value %u\n", nr_run_stat);
 					break;
 			}
-		} //else
-			//pr_info("intelli_plug is suspened!\n");
+		}
 	}
 	schedule_delayed_work_on(0, &intelli_plug_work,
 		msecs_to_jiffies(DEF_SAMPLING_MS));
@@ -233,7 +236,7 @@ static void __cpuinit intelli_plug_work_fn(struct work_struct *work)
 static void intelli_plug_early_suspend(struct early_suspend *handler)
 {
 	int i;
-	int num_of_active_cores = 4;
+	int num_of_active_cores = 2;
 	
 	cancel_delayed_work_sync(&intelli_plug_work);
 
@@ -260,10 +263,7 @@ static void __cpuinit intelli_plug_late_resume(struct early_suspend *handler)
 	mutex_unlock(&intelli_plug_mutex);
 
 	/* wake up everyone */
-	if (eco_mode_active)
-		num_of_active_cores = 2;
-	else
-		num_of_active_cores = 4;
+	num_of_active_cores = 2;
 
 	for (i=1; i<num_of_active_cores; i++) {
 		if (!cpu_online(i))
@@ -283,13 +283,17 @@ static struct early_suspend intelli_plug_early_suspend_struct_driver = {
 
 int __init intelli_plug_init(void)
 {
-	/* We want all CPUs to do sampling nearly on same jiffy */
-	int delay = usecs_to_jiffies(DEF_SAMPLING_RATE);
+	int ret, delay;
+
+	ret = init_rq_avg();
+	if (ret) return ret;
+
+	/* we want all CPUs to do sampling nearly on same jiffy */
+	delay = usecs_to_jiffies(DEF_SAMPLING_RATE);
 
 	if (num_online_cpus() > 1)
 		delay -= jiffies % delay;
 
-	//pr_info("intelli_plug: scheduler delay is: %d\n", delay);
 	pr_info("intelli_plug: version %d.%d by faux123\n",
 		 INTELLI_PLUG_MAJOR_VERSION,
 		 INTELLI_PLUG_MINOR_VERSION);
