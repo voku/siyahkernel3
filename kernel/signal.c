da510b3cac3864aa7ebddc0d37f3e1331cc6ec0e
Vineet Gupta <vgupta@synopsys.com>
2013-04-30 15:27:19 -0700

    arc, print-fatal-signals: reduce duplicated information
    After the recent generic debug info on dump_stack() and friends, arc
    is printing duplicate information on debug dumps.
    
     [ARCLinux]$ ./crash
     crash/50: potentially unexpected fatal signal 11.	<-- [1]
     /sbin/crash, TGID 50					<-- [2]
     Pid: 50, comm: crash Not tainted 3.9.0-rc4+ #132 	<-- [3]
     ...
    
    Remove them.
    
    [tj@kernel.org: updated patch desc]
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Jesper Nilsson <jesper.nilsson@axis.com>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Mike Frysinger <vapier@gentoo.org>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1162,8 +1162,7 @@ static int send_signal(int sig, struct siginfo *info, struct task_struct *t,
 
 static void print_fatal_signal(struct pt_regs *regs, int signr)
 {
-	printk(KERN_INFO "%s/%d: potentially unexpected fatal signal %d.\n",
-		current->comm, task_pid_nr(current), signr);
+	printk(KERN_INFO "potentially unexpected fatal signal %d.\n", signr);
 
 #if defined(__i386__) && !defined(__arch_um__)
 	printk(KERN_INFO "code at %08lx: ", regs->ip);
