56af00fddf04b22ad14e8af6161105241030abeb
Paolo Valente <paolo.valente@unimore.it>
2013-05-30 18:18:26 +0200

    block: Switch from BFQ-v6r1 for 3.9.0 to BFQ-v6r2 for 3.9.0.
    Improvements:
    
    - Fairness fix: the case of queue expiration for budget timeout is
      now correctly handled also for sync queues, thus allowing also
      the processes corresponding to these queues to be guaranteed their
      reserved share of the disk throughput.
    - Fixed a bug that prevented group weights from being correctly
      set via the sysfs interface.
    - Fixed a bug that cleared a previously-set group weight if the
      same value was re-inserted via the sysfs interface.
    - Fixed an EQM bug that allowed a newly-started process to skip
      its initial weight-raising period if its queue was merged before
      its first request was inserted.
    - Fixed a bug that preserved already-started weight-raising periods
      even if the low_latency tunable was disabled.
    - The raising_max_time tunable now shows, more user-friendly, the
      maximum raising time in milliseconds.
    
    Signed-off-by: Paolo Valente <paolo.valente@unimore.it>
    Signed-off-by: Arianna Avanzini <avanzini.arianna@gmail.com>
    
    Conflicts:
    	block/bfq.h

--- block/bfq.h
+++ block/bfq.h
@@ -1,5 +1,5 @@
 /*
- * BFQ-v5r1 for 3.4.0: data structures and common functions prototypes.
+ * BFQ-v6r2 for 3.9.0: data structures and common functions prototypes.
  *
  * Based on ideas and code from CFQ:
  * Copyright (C) 2003 Jens Axboe <axboe@kernel.dk>
@@ -273,6 +273,7 @@ struct bfq_io_cq {
 	struct io_cq icq; /* must be the first member */
 	struct bfq_queue *bfqq[2];
 	struct bfq_ttime ttime;
+	int ioprio;
 
 	unsigned int raising_time_left;
 	unsigned int saved_idle_window;
@@ -605,13 +606,14 @@ static inline void bfq_put_bfqd_unlock(struct bfq_data *bfqd,
 	spin_unlock_irqrestore(bfqd->queue->queue_lock, *flags);
 }
 
-static void bfq_changed_ioprio(struct io_context *ioc,
-			       struct bfq_io_cq *bic);
+static void bfq_changed_ioprio(struct bfq_io_cq *bic);
 static void bfq_put_queue(struct bfq_queue *bfqq);
 static void bfq_dispatch_insert(struct request_queue *q, struct request *rq);
 static struct bfq_queue *bfq_get_queue(struct bfq_data *bfqd,
 				       struct bfq_group *bfqg, int is_sync,
-				       struct io_context *ioc, gfp_t gfp_mask);
+				       struct bfq_io_cq *bic, gfp_t gfp_mask);
+static void bfq_end_raising_async_queues(struct bfq_data *bfqd,
+					 struct bfq_group *bfqg);
 static void bfq_put_async_queues(struct bfq_data *bfqd, struct bfq_group *bfqg);
 static void bfq_exit_bfqq(struct bfq_data *bfqd, struct bfq_queue *bfqq);
 #endif
