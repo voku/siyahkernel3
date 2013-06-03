7553328af8c25523ab0304d90da4d0accc622c84
Yuri.Sh <yuri@bynet.co.il>
2013-06-03 12:03:22 +0300

    MALIv2: I have found BIG bug in driver config. and changed names to default! As they called in kernel/power/main.c and tmu.c in exynos
    Now should be OK.

--- drivers/media/video/samsung/ump/linux/ump_kernel_linux.c
+++ drivers/media/video/samsung/ump/linux/ump_kernel_linux.c
@@ -92,7 +92,7 @@ static int ump_file_ioctl(struct inode *inode, struct file *filp, unsigned int c
 #endif
 static int ump_file_mmap(struct file * filp, struct vm_area_struct * vma);
 
-#ifdef CONFIG_VIDEO_MALI400MP_UMP
+#if defined(CONFIG_VIDEO_UMP)
 extern int map_errcode( _mali_osk_errcode_t err );
 #endif
 
@@ -394,7 +394,7 @@ static int ump_file_ioctl(struct inode *inode, struct file *filp, unsigned int c
 	return err;
 }
 
-#ifndef CONFIG_VIDEO_MALI400MP_UMP
+#ifndef CONFIG_VIDEO_UMP
 int map_errcode( _mali_osk_errcode_t err )
 {
     switch(err)
