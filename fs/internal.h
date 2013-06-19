105ef7e6c9c59562de474a31633064c78fa667fd
Al Viro <viro@zeniv.linux.org.uk>
2013-06-19 15:26:04 +0400

    constify rw_verify_area()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/internal.h
+++ fs/internal.h
@@ -131,6 +131,7 @@ extern int d_set_mounted(struct dentry *dentry);
  * read_write.c
  */
 extern ssize_t __kernel_write(struct file *, const char *, size_t, loff_t *);
+extern int rw_verify_area(int, struct file *, const loff_t *, size_t);
 
 /*
  * splice.c
