3a764223a5514756e9cd9e31a244d8944a3b4b7c
Yuri.Sh <yuri@bynet.co.il>
2013-07-16 14:43:56 +0300

    Full sync for kernel/signal.c from 3.11.y kernel.

--- kernel/signal.c
+++ kernel/signal.c
@@ -2392,6 +2392,12 @@ void signal_delivered(int sig, siginfo_t *info, struct k_sigaction *ka,
 {
 	sigset_t blocked;
 
+	/* A signal was successfully delivered, and the
+	   saved sigmask was stored on the signal frame,
+	   and will be restored by sigreturn.  So we can
+	   simply clear the restore sigmask flag.  */
+	clear_restore_sigmask();
+
 	sigorsets(&blocked, &current->blocked, &ka->sa.sa_mask);
 	if (!(ka->sa.sa_flags & SA_NODEFER))
 		sigaddset(&blocked, sig);
