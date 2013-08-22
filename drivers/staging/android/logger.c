077f236c5571d84fdcec4ae4f96c141200a0d3ec
Dorimanx <yuri@bynet.co.il>
2013-08-22 18:10:48 +0300

    logger: Add an enable toggle for entry storage, TNX @AndreiLux
    https://github.com/AndreiLux/Perseus-UNIVERSAL5410/commit/7c1d084c6ba3cf6b0710527cdba550e3af8c29e5
    
    veru cool and simple. :)

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -34,6 +34,9 @@
 #include <asm/ioctls.h>
 #include <mach/sec_debug.h>
 
+static unsigned int enabled = 1;
+module_param(enabled, uint, S_IWUSR | S_IRUGO);
+
 /**
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
  * @buffer:	The actual ring buffer
@@ -488,6 +491,9 @@ static ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct logger_entry header;
 	struct timespec now;
 	ssize_t ret = 0;
+
+	if (!enabled)
+		return 0;
 
 	now = current_kernel_time();
 
