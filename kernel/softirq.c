e3b96f37758c27441f74000b637395002a844803
Dorimanx <yuri@bynet.co.il>
2013-09-14 13:07:23 +0300

    Remove GENERIC_HARDIRQ config option
    After the last architecture switched to generic hard irqs the config
    options HAVE_GENERIC_HARDIRQS & GENERIC_HARDIRQS and the related code
    for !CONFIG_GENERIC_HARDIRQS can be removed.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -883,7 +883,6 @@ int __init __weak early_irq_init(void)
 	return 0;
 }
 
-#ifdef CONFIG_GENERIC_HARDIRQS
 int __init __weak arch_probe_nr_irqs(void)
 {
 	return NR_IRQS_LEGACY;
@@ -893,4 +892,3 @@ int __init __weak arch_early_irq_init(void)
 {
 	return 0;
 }
-#endif
