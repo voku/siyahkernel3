5ec0b89665db677d773a9eb8a2306785058a9755
Dorimanx <yuri@bynet.co.il>
2013-09-14 22:53:57 +0300

    Cleanups to arch/arm/kernel/setup.c

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -435,13 +435,13 @@ u32 __cpu_logical_map[NR_CPUS] = { [0 ... NR_CPUS-1] = MPIDR_INVALID };
 
 void __init smp_setup_processor_id(void)
 {
-        int i;
+	int i;
 	u32 mpidr = is_smp() ? read_cpuid_mpidr() & MPIDR_HWID_BITMASK : 0;
 	u32 cpu = MPIDR_AFFINITY_LEVEL(mpidr, 0);
 
-        cpu_logical_map(0) = cpu;
-        for (i = 1; i < nr_cpu_ids; ++i)
-                cpu_logical_map(i) = i == cpu ? 0 : i;
+	cpu_logical_map(0) = cpu;
+	for (i = 1; i < nr_cpu_ids; ++i)
+		cpu_logical_map(i) = i == cpu ? 0 : i;
 
 	/*
 	 * clear __my_cpu_offset on boot CPU to avoid hang caused by
