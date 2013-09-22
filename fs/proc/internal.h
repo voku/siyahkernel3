ca855581af32019cf41f290d9290f748c6229428
Dorimanx <yuri@bynet.co.il>
2013-09-22 22:05:48 +0300

    SYNC TTY driver build config. no real code change.

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -224,7 +224,11 @@ static inline void sysctl_head_put(struct ctl_table_header *head) { }
 /*
  * proc_tty.c
  */
+#ifdef CONFIG_TTY
 extern void proc_tty_init(void);
+#else
+static inline void proc_tty_init(void) {}
+#endif
 
 /*
  * root.c
