1c5f9c1a6f3080c3be438ee152076fb13916abec
Computoncio <computoncio@gmail.com>
2013-10-18 14:50:33 -0300

    Fixed centered text on boot logo

--- mod_logo_build.sh
+++ mod_logo_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 GETVER=`grep 'Siyah-.*-V' .config |sed 's/Siyah-//g' | sed 's/.*".//g' | sed 's/-J.*//g'`;
 boot_image=/tmp/initramfs_source/res/images/icon_clockwork.png;
-convert -ordered-dither threshold,32,64,32 -pointsize 17 -fill white -draw "text 104,770 \"$GETVER [`date "+%H:%M | %d.%m.%Y"| sed -e ' s/\"/\\\"/g' `]\"" $boot_image $boot_image;
+convert -ordered-dither threshold,32,64,32 -pointsize 17 -fill white -gravity center -draw "text 0,380 \"$GETVER [`date "+%H:%M | %d.%m.%Y"| sed -e ' s/\"/\\\"/g' `]\"" $boot_image $boot_image;
 #to gain more 100k for kernel stuff uncomment next line, but it's will take time to convert.
 #optipng -o7 -quiet $boot_image;
 echo "1" > $TMPFILE;
